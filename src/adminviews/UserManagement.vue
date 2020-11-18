7115ca9c735bec07b57b33ede0ca2fa01cb3fdab
葬我于高山之上兮 <55906505+jbc2212321@users.noreply.github.com>
2020-11-18 10:34:30 +0800

    11/18号更新 查看病人或者医生的详细信息

--- src/adminviews/UserManagement.vue
+++ src/adminviews/UserManagement.vue
@@ -4,6 +4,7 @@
 
   <el-table
     ref="filterTable"
+    height="600px"
     :data="PageData"
     @filter-change="handleFilterChange"
     stripe
@@ -46,7 +47,7 @@
       width="200">
       <template slot-scope="scope">
         <el-button
-          @click.native.prevent="deleteRow(scope.$index, PageData)"
+          @click.native.prevent="lookRow(scope.$index, PageData)"
           size="small">
           查看
         </el-button>
@@ -65,13 +66,115 @@
     @size-change="handleSizeChange"
     @current-change="handleCurrentChange"
     :current-page="currentPage"
-    :page-sizes="[5, 10, 20, 50]"
+    :page-sizes="[5, 9, 15, 20]"
     :page-size="10"
     layout="total, sizes, prev, pager, next, jumper"
     :total="tableData.length"
 
   >
   </el-pagination>
+
+<!--  医生表格-->
+  <el-dialog
+    :visible.sync="dialogVisible"
+    width="60%"
+    :before-close="handleClose">
+      <span><table border="1"
+                   cellspacing="0px"
+                   style="margin:auto;"
+                   width="800px">
+    <tr height="50"  style="text-align: center;">
+        <td rowspan="2" >姓名</td>
+        <td rowspan="2" width="100">{{doctorName}}</td>
+        <td width="100">性别</td>
+        <td width="100">{{sex}}</td>
+        <td>医生编号</td>
+        <td width="100">{{doctorID}}</td>
+        <td  rowspan="3" width="120">照片</td>
+    </tr>
+    <tr height="50" style="text-align: center;">
+        <td>医生账户</td>
+        <td>{{ daccountNo }}</td>
+        <td>科室编号</td>
+        <td>{{departmentNO}}</td>
+    </tr>
+    <tr height="50" style="text-align: center;">
+        <td width="100">电话号码</td>
+        <td colspan="2">{{phoneNO }}</td>
+        <td>出生年月</td>
+        <td colspan="2">{{ birthday }}</td>
+    </tr>
+    <tr height="50" style="text-align: center;">
+        <td>国籍</td>
+        <td colspan="2">{{nationality}}</td>
+        <td>籍贯</td>
+        <td width="100">{{nativePlace}}</td>
+        <td>民族</td>
+        <td>{{nation}}</td>
+    </tr>
+    <tr height="50" style="text-align: center;">
+        <td>毕业院校</td>
+        <td colspan="2">{{college}}</td>
+        <td>现居住地址</td>
+        <td colspan="3">{{address}}</td>
+    </tr>
+    <tr height="50" style="text-align: center;">
+        <td rowspan="2">擅长领域</td>
+        <td colspan="6" rowspan="2">{{expertise}}</td>
+    <tr height="50" style="text-align: center;">
+    </tr>
+    <tr height="40" style="text-align: center;">
+        <td rowspan="2">个人著作</td>
+        <td colspan="6" rowspan="2">{{works}}</td>
+    <tr height="40" style="text-align: center;">
+    </tr>
+    <tr height="80" style="text-align: center;">
+        <td rowspan="2">个人简介</td>
+        <td colspan="6" rowspan="2">{{introduction}}</td>
+    <tr height="80" style="text-align: center;">
+    </tr>
+    <tr height="50" style="text-align: center;">
+        <td rowspan="2">个人成就</td>
+        <td colspan="6" rowspan="2">{{achievements}}</td>
+    <tr height="50" style="text-align: center;">
+    </tr>
+    <tr height="80" style="text-align: center;">
+        <td rowspan="2">社会评价</td>
+        <td colspan="6" rowspan="2">{{evaluation}}</td>
+    <tr height="80" style="text-align: center;">
+    </tr>
+</table></span>
+  </el-dialog>
+
+<!--病人表格  -->
+  <el-dialog
+    :visible.sync="dialogVisibleP"
+    width="60%"
+    :before-close="handleClose">
+      <span><table border="1"
+                   cellspacing="0px"
+                   style="margin:auto;"
+                   width="800px">
+    <tr height="50"  style="text-align: center;">
+        <td rowspan="2" >姓名</td>
+        <td rowspan="2" width="100">{{patientName}}</td>
+        <td rowspan="2" width="100" >性别</td>
+        <td rowspan="2" width="100">{{sex}}</td>
+        <td width="100">病人编号</td>
+        <td width="100">{{patientID}}</td>
+    </tr>
+    <tr height="50" style="text-align: center;">
+        <td>账户编号</td>
+        <td>{{paccountNo}}</td>
+    </tr>
+    <tr height="50" style="text-align: center;">
+        <td width="100">电话号码</td>
+        <td colspan="2">{{phoneNO}}</td>
+        <td>出生年月</td>
+        <td colspan="2">{{birthday}}</td>
+    </tr>
+</table></span>
+  </el-dialog>
 </div>
 </template>
 
@@ -82,19 +185,45 @@ name: "UserManagement",
     return {
       currentPage:1,//默认开始页面
       count:0,//筛选
-      pagesize:10,
+      pagesize:9,
       AllAccount:[],
       filters:[{ text: '病患', value: '病患' }, { text: '医师', value: '医师' }],
       filter:"",
+      dialogVisible: false,
+      dialogVisibleP:false,
+    //  接下来是医生表格的数据
+      doctorID:"",
+      departmentNO:"",
+      daccountNo:"",
+      doctorName:"",
+      phoneNO:"",
+      address:"",
+      birthday:"",
+      sex:"",
+      introduction:"",
+      expertise:"",
+      achievements:"",
+      nationality:"",
+      nation:"",
+      college:"",
+      nativePlace:"",
+      works:"",
+      evaluation:"",
+      //
+      //这里是病人的详细信息
+      patientID:"",
+      paccountNo:"",
+      patientName:"",
+
+
+      //
+
     }
   },
   computed:{
       tableData:function (){
         return this.AllAccount.filter(item => {
-
           if (this.filter.length===1) {
-            // console.log("标签"+this.filter[0])
-            // console.log(item["tag"])
             if (item["tag"]!==this.filter[0]) {
               return false
             }
@@ -119,7 +248,51 @@ name: "UserManagement",
     filterTag(value, row) {
       return row.tag === value;
     },
-    deleteRow(index, tableData){},
+    lookRow(index,tableData){
+      this.$axios({
+        url: "http://localhost:8096/getInformationDocOrPatient",
+        method: "post",
+        data:{
+          phone:tableData[index]["tel"],
+          tag:tableData[index]["tag"],
+        }
+      }).then(res=>{
+        if(tableData[index]["tag"]==="医师"){
+          this.doctorName=res.data["doctorName"]
+          this.evaluation=res.data["evaluation"]
+          this.doctorID=res.data["doctorID"]
+          this.departmentNO=res.data["departmentNO"]
+          this.daccountNo=res.data["daccountNo"]
+          this.phoneNO=tableData[index]["tel"] //这里为什么出错
+          this.address=res.data["address"]
+          this.birthday=res.data["birthday"]
+          this.sex=res.data["sex"]
+          this.introduction=res.data["introduction"]
+          this.expertise=res.data["expertise"]
+          this.achievements=res.data["achievements"]
+          this.nationality=res.data["nationality"]
+          this.nation=res.data["nation"]
+          this.college=res.data["college"]
+          this.nativePlace=res.data["nativePlace"]
+          this.works=res.data["works"]
+          this.evaluation=res.data["evaluation"]
+          this.dialogVisible=true
+        }
+        else {
+          this.patientID=res.data["patientID"]
+          this.paccountNo=res.data["paccountNo"]
+          this.patientName=res.data["patientName"]
+          this.phoneNO=tableData[index]["tel"]
+          this.birthday=res.data["birthday"]
+          this.sex=res.data["sex"]
+          this.dialogVisibleP=true
+        }
+      })
+
+    },
+    deleteRow(index, tableData){
+
+    },
     handleSizeChange(val) {
       this.currentPage = 1
       this.pagesize=val
@@ -129,7 +302,13 @@ name: "UserManagement",
     },
     handleFilterChange(value) {
       this.filter=value.status
-      // console.log(value.status)
+    },
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done();
+        })
+        .catch(_ => {});
     }
   }
 }
