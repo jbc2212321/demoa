92f039a576f73320812a5c813955dca2c7b17d66
葬我于高山之上兮 <55906505+jbc2212321@users.noreply.github.com>
2021-01-02 14:26:17 +0800

    1月2号 14：26更新 上传限制大小

--- src/adminviews/UploadCsv.vue
+++ src/adminviews/UploadCsv.vue
@@ -1,15 +1,15 @@
 <template>
     <div id="UploadCsv">
         <template>
-<!--            <span class="demonstration">科室：</span>-->
-<!--            <el-select v-model="value0" placeholder="选择科室" >-->
-<!--                <el-option-->
-<!--                        v-for="item in offices"-->
-<!--                        :key="item.value"-->
-<!--                        :label="item.label"-->
-<!--                        :value="item.value">-->
-<!--                </el-option>-->
-<!--            </el-select>-->
+            <!--            <span class="demonstration">科室：</span>-->
+            <!--            <el-select v-model="value0" placeholder="选择科室" >-->
+            <!--                <el-option-->
+            <!--                        v-for="item in offices"-->
+            <!--                        :key="item.value"-->
+            <!--                        :label="item.label"-->
+            <!--                        :value="item.value">-->
+            <!--                </el-option>-->
+            <!--            </el-select>-->
         </template>
         <br><br>
         <el-upload
@@ -21,14 +21,16 @@
                 :on-success="handle_success"
                 :before-remove="beforeRemove"
                 :show-file-list="false"
+                :beforeUpload="beforeAvatarUpload"
                 :on-exceed="handleExceed">
-<!--            <el-button size="small" type="primary" @click="beforeUpload" >点击上传</el-button>-->
-<!--            <div slot="tip" class="el-upload__tip">只能上传csv文件</div>-->
+
+            <!--            <el-button size="small" type="primary" @click="beforeUpload" >点击上传</el-button>-->
+            <!--            <div slot="tip" class="el-upload__tip">只能上传csv文件</div>-->
             <el-button id="initSlide" type="primary">点击上传</el-button>
             <div slot="tip" class="el-upload__tip">只能上传csv文件</div>
         </el-upload>
 
-<!--        <input type="file" accept="*.csv">上传-->
+        <!--        <input type="file" accept="*.csv">上传-->
 
 
     </div>
@@ -37,9 +39,9 @@
 <script>
   export default {
     name: 'UploadCsv',
-    data() {
+    data () {
       return {
-        url:"",
+        url: '',
         offices: [{
           value: '口腔科',
           label: '口腔科',
@@ -49,33 +51,41 @@
         }],
         value0: '',//科室
 
-      };
+      }
     },
-    mounted() {
+    mounted () {
       this.$notify({
         title: '提示',
         message: '血液科csv请以B开头命名，口腔科请以T开头命名',
         duration: 0
-      });
+      })
     },
     methods: {
-      handle_success(res){
-        // console.log("返回值",res)
-        if(res==="命名不正确"){
-            this.$message({
-              showClose: true,
-              message: '命名不正确！',
-              type: 'error'
-            })
+      beforeAvatarUpload (file) {
+        const isLt2M = file.size / 1024 / 1024 < 1
+        if (!isLt2M) {
+          this.$message({
+            message: '上传文件大小不能超过 1MB!',
+            type: 'warning'
+          })
         }
-        else if(res==="表名与csv内容冲突"){
+        return isLt2M
+      },
+      handle_success (res) {
+        // console.log("返回值",res)
+        if (res === '命名不正确') {
+          this.$message({
+            showClose: true,
+            message: '命名不正确！',
+            type: 'error'
+          })
+        } else if (res === '表名与csv内容冲突') {
           this.$message({
             showClose: true,
             message: '表名与csv内容冲突！',
             type: 'error'
           })
-        }
-        else if(res==="上传成功"){
+        } else if (res === '上传成功') {
           this.$message({
             showClose: true,
             message: '上传成功！',
@@ -83,8 +93,8 @@
           })
         }
       },
-      judge(){
-        if (this.value0===""){
+      judge () {
+        if (this.value0 === '') {
           this.$message({
             showClose: true,
             message: '请选择科室！',
@@ -94,35 +104,35 @@
         }
       },
       async beforeUpload () {
-        if (this.value0 === "") {
+        if (this.value0 === '') {
           this.$message({
             showClose: true,
             message: '请选择科室！',
             type: 'warning'
           })
-          return;
+
         }
 
       },
-      handleRemove(file, fileList) {
-        console.log(file, fileList);
+      handleRemove (file, fileList) {
+        console.log(file, fileList)
       },
-      handlePreview(file) {
+      handlePreview (file) {
 
-        console.log(file);
+        console.log(file)
       },
-      handleExceed(files, fileList) {
-        this.$message.warning(`当前限制选择 1 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+      handleExceed (files, fileList) {
+        this.$message.warning(`当前限制选择 1 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`)
       },
-      beforeRemove(file, fileList) {
-        return this.$confirm(`确定移除 ${ file.name }？`);
+      beforeRemove (file, fileList) {
+        return this.$confirm(`确定移除 ${file.name}？`)
       }
     }
   }
 </script>
 
 <style scoped>
-        #UploadCsv{
-            padding:20px 0 0 20px;
-        }
+    #UploadCsv {
+        padding: 20px 0 0 20px;
+    }
 </style>
